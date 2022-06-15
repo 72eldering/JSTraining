957d6ac02dd49f9fbae9c0697e73329da4d8192e
eldering <72eldering@gmail.com>
2022-06-15 10:44:29 +0300

    fix err JS_HW_1Z4.js

--- JS_HW_1Z4.js
+++ JS_HW_1Z4.js
@@ -5,12 +5,9 @@ let age_1 = prompt('Введи пару циферок:')
 let age_2 = prompt( 'А ещё пару:')
 let age_3 = prompt('И ещё парочку:')
 
-age_1 = Number(age_1)
-age_2 = Number(age_2)
-age_3 = Number(age_3)
-
 function age(f_age1, f_age2, f_age3){
-    if (isNaN(age_1) || isNaN(age_2) || isNaN(age_3)){
+    if (isNaN(+f_age1) || isNaN(+f_age2) || isNaN(+f_age3) ||
+    f_age1 == '' || f_age2 == '' || f_age3 == ''){
         return 'Ошибка!!! Нужны числа'
      } else if (f_age1 < f_age2) {
          return 'You don’t have access cause your age is ' + f_age1 + ' It’s less then ' + f_age2
